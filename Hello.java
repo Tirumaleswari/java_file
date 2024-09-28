aa4d22ebebaa5ad9b0d7cce3df22128baba334db
Tirumaleswari <228A1A0575@risekrishnasaiprakasam.edu.in>
2024-09-28 15:50:45 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package sample_java_project;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world this is robo");
+
+	}
+
+}
